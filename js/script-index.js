34c43051a384c35a95fefe42a6a5a338d812d344
yasvalenzuela <yasna.valenzuela91@gmail.com>
2018-01-08 21:08:45 -0300

    Etapa 6: creando html desde js

--- js/script-index.js
+++ js/script-index.js
@@ -91,7 +91,28 @@ function renderActivities(activities) {
 * archivo "templates/templates-activity.html"
 */
 function renderActivity(recipe) {
+  /*
+  * creo la estructura html de template-activity.html
+  */
+  $('.list-activities').append('<a href="#" class="item-activity">' +
 
+  '<span class="attribution">' +
+  
+    '<span class="avatar">' +
+      '<img src=' + recipe.userAvatar + 'class="image-avatar">' +
+    '</span>' +
+      
+    '<span class="meta">' +
+      '<span class="author">' + recipe.userName + '</span>' + 'made' +
+      '<span class="recipe">' + recipe.recipeName + '</span>'+ ':' + recipe.text + 
+      '<span class="location">' + '&mdash;' + recipe.place + '</span>' +
+    '</span>' +
+  
+  '</span>' +
+
+  '<div class="bg-image" style="background-image: url(' + recipe.image + ');">' + '</div>' +
+
+'</a>');
 }
 
 
